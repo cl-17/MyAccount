5ecb7d6f954b0ddb3e7ec9986c4446d372c393eb
vagrant <vagrant@localhost.localdomain>
2018-12-13 00:00:27 +0900

    Delete Unnecessary Files

--- myacct/myapp/Master/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def master(request):
-    return render(request, 'master.html')
-
-def classification(request):
-    return render(request, 'classification.html')
-
